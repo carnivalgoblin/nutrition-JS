c99d461cc2e3a2af13204470503043ae4b6466e3
Rico Prodan <rico.prodan@gmail.com>
2023-01-05 13:10:13 +0100

    added nutrient calculation

--- src/controllers/ProductList.js
+++ src/controllers/ProductList.js
@@ -1,9 +1,9 @@
-
 "use strict"
 
 const { info } = require("../api/product")
 const { on } = require("../utils/dom")
 const addProductTemplate = require("../templates/ProductList/addProduct.ejs")
+const EventEmitter = require("eventemitter3")
 
 /**
  * 
@@ -11,8 +11,10 @@ const addProductTemplate = require("../templates/ProductList/addProduct.ejs")
  */
 function ProductList(listElement) {
     this.products = []
+
     this.listElement = listElement
 
+    this.events = new EventEmitter()
 }
 
 ProductList.prototype.init = function () {
@@ -20,13 +22,79 @@ ProductList.prototype.init = function () {
         const fdcId = parseInt(event.handleObj.getAttribute("data-fdcid"), 10)
         this.removeProduct(fdcId)
     })
+
+    on(".product-search__amount", "change", (event) => {
+        const fdcId = parseInt(event.handleObj.getAttribute("data-fdcid"), 10)
+        const value = parseInt(event.handleObj.value, 10)
+        this.updateAmount(fdcId, value)
+    })
+}
+
+ProductList.prototype.emitNutrients = function () {
+    const nutrients = this.getNutrients()
+    this.events.emit("nutrientChange", nutrients)
+}
+
+ProductList.prototype.getNutrientsForProduct = function (product) {
+    const nutrients = {
+        carbs: 0,
+        protein: 0,
+        fat: 0
+    }
+
+    for (const foodNutrient of product.product.foodNutrients) {
+        if (("" + foodNutrient.nutrient.number) === "205") {
+            nutrients.carbs = foodNutrient.amount
+        } else if (("" + foodNutrient.nutrient.number) === "204") {
+            nutrients.fat = foodNutrient.amount
+        } else if (("" + foodNutrient.nutrient.number) === "203") {
+            nutrients.protein = foodNutrient.amount
+        }
+    }
+
+    return {
+        carbs: (nutrients.carbs / 100) * product.amount,
+        protein: (nutrients.protein / 100) * product.amount,
+        fat: (nutrients.fat / 100) * product.amount
+    }
+
+
+}
+
+ProductList.prototype.getNutrients = function () {
+    const nutrients = {
+        carbs: 0,
+        protein: 0,
+        fat: 0
+    }
+
+    for (const product of this.products) {
+        const productNutrients = this.getNutrientsForProduct(product)
+
+        nutrients.carbs += productNutrients.carbs
+        nutrients.protein += productNutrients.protein
+        nutrients.fat += productNutrients.fat
+    }
+
+    return nutrients
+}
+
+ProductList.prototype.updateAmount = function (fdcId, value) {
+    for (const product of this.products) {
+        if (product.product['fdcId'] === fdcId) {
+            product.amount = value
+            break
+        }
+    }
+    this.emitNutrients()
+    console.log("updateAmount:", this.products)
 }
 
 ProductList.prototype.removeProduct = function (fdcId) {
     let index = null
     for (const i in this.products) {
         const product = this.products[i]
-        if (product['fdcId'] === fdcId) {
+        if (product.product['fdcId'] === fdcId) {
             index = i
             break
         }
@@ -39,13 +107,18 @@ ProductList.prototype.removeProduct = function (fdcId) {
         )
         if (trElement) trElement.remove()
     }
+    this.emitNutrients()
     console.log("this.products:", this.products)
 }
 
 ProductList.prototype.addProduct = function (fdcId) {
     info(fdcId)
         .then((product) => {
-            this.products.push(product)
+
+            this.products.push({
+                amount: 100,
+                product
+            })
             console.log("this.products:", this.products)
 
             const productHtml = addProductTemplate({
@@ -56,6 +129,10 @@ ProductList.prototype.addProduct = function (fdcId) {
             // this.listElement.innerHTML = this.listElement.innerHTML + productHtml
             this.listElement.insertAdjacentHTML("beforeend", productHtml)
             // console.log(productHtml)
+
+            // this.getNutrients()
+
+            this.emitNutrients()
         })
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require("../scss/index.scss")
 
 const ProductSearch = require("./controllers/ProductSearch")
 const ProductList = require("./controllers/ProductList")
+const ProductNutrients = require("./controllers/ProductNutrients")
 
 const productSearch = new ProductSearch(
     document.getElementById("productSearchInput"),
@@ -21,6 +22,19 @@ productList.init()
 
 productList.addProduct(2011468)
 
+const productNutrients = new ProductNutrients(
+    document.getElementById("productNutrient--carbs"),
+    document.getElementById("productNutrient--protein"),
+    document.getElementById("productNutrient--fat")
+)
+productNutrients.init()
+
+
+
 productSearch.events.on("productSelected", (fdcId) => {
     productList.addProduct(fdcId)
+})
+
+productList.events.on("nutrientChange", (nutrients) => {
+    productNutrients.setNutrients(nutrients)
 })
